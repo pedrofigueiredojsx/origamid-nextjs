04d52af3dd0b2ae12b99d0f88cdefb0f1fff8966
Pedro Figueiredo <pedrofigueiredojs@gmail.com>
2025-11-21 13:54:53 -0300

    feat: ssr use cliente use server e componente em componente component nested

---
+++ server-e-cliente-component/app/sobre/page.tsx
@@ -0,0 +1,12 @@
+import dynamic from 'next/dynamic'
+
+const Width = dynamic(() => import('@/components/width'), { ssr: false })
+
+export default function SobrePage() {
+  return (
+    <main>
+      <h1>Sobre</h1>
+      <Width />
+    </main>
+  )
+}
