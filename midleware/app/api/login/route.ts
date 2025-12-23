613b5b167b76d311cfc8113b59ac012afa1ae2f6
Pedro Figueiredo <pedrofigueiredojs@gmail.com>
2025-12-23 14:04:03 -0300

    feat: middleware

---
+++ midleware/app/api/login/route.ts
@@ -0,0 +1,36 @@
+import { cookies } from 'next/headers'
+import { NextRequest } from 'next/server'
+
+export async function POST(request: NextRequest) {
+  const body = (await request.json()) as { username: string; password: string }
+
+  const response = await fetch('https://api.origamid.online/conta/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      username: body.username,
+      password: body.password,
+    }),
+  })
+
+  if (!response.ok) {
+    return Response.json(
+      { autorizado: false, erro: 'Dados incorretos' },
+      { status: 401 }
+    )
+  }
+
+  const data = await response.json()
+  const cookieStore = await cookies()
+
+  cookieStore.set('token', data.token, {
+    httpOnly: true,
+    secure: true,
+    // sameSite: 'lax',
+    // maxAge: 60 * 60 * 24 * 7,
+  })
+
+  return Response.json({ autorizado: true })
+}
