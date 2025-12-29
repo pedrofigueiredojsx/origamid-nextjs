76f8c14d999983fea4b1c3f7fdeba0302498274d
Pedro Figueiredo <pedrofigueiredojs@gmail.com>
2025-12-29 14:36:21 -0300

    feat: footer

--- dogs-next/src/app/layout.tsx
+++ dogs-next/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 import { type_second } from '@/functions/fonts'
 import Header from '@/components/header'
+import Footer from '@/components/footer'
 
 export const metadata: Metadata = {
   title: 'Dogs Next',
@@ -15,8 +16,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <Header />
-      <body className={type_second.variable}>{children}</body>
+      <body className={type_second.variable}>
+        <div className="App">
+          <Header />
+          <main className="AppBody">{children}</main>
+          <Footer />
+        </div>
+      </body>
     </html>
   )
 }
