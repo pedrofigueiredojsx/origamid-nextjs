bb9f9dd66b7305414d7c0f1d87fc91ec91d4ea4b
Pedro Figueiredo <pedrofigueiredojs@gmail.com>
2026-01-02 19:16:18 -0300

    feat: login functions etc

--- dogs-next/src/components/login/login-form.tsx
+++ dogs-next/src/components/login/login-form.tsx
@@ -1,12 +1,37 @@
-import login from '@/actions/login'
+'use client'
 
-export async function LoginForm() {
+import login from '@/actions/login'
+import { useFormState, useFormStatus } from 'react-dom'
+import Button from '../forms/button'
+
+function FormButton() {
+  const { pending } = useFormStatus()
+
   return (
     <>
-      <form action={login}>
+      {pending ? (
+        <Button disabled={pending}>Enviando...</Button>
+      ) : (
+        <Button disabled={pending}>Entrar</Button>
+      )}
+    </>
+  )
+}
+
+export function LoginForm() {
+  const [state, action] = useFormState(login, {
+    ok: false,
+    error: '',
+    data: null,
+  })
+
+  return (
+    <>
+      <form action={action}>
         <input type="text" name="username" placeholder="usuário" />
         <input type="password" placeholder="senha" />
-        <button>Entrar</button>
+        <FormButton />
+        <p>{state.error}</p>
       </form>
     </>
   )
